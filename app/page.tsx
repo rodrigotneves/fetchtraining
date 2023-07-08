f202d5ba149c916fd21f0d26955c10fd10f9c92d
rodrigotneves <thiago2210933@aluno.faculdadedescomplica.com.br>
2023-07-08 01:40:56 -0300

    first

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <p>
+        <Link href="/users ">Users</Link>
+      </p>
+    </main>
+  );
+}
